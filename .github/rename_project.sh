2b6d0eef9916634c16afa1e86941c1fb398d27a5
niski1996 <niski1996@users.noreply.github.com>
2025-05-25 17:49:38 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="niski1996"
+original_name="tmp_pyth"
+original_urlname="tmp-pyth"
+original_description="Awesome tmp_pyth created by niski1996"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv tmp_pyth $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tmp_pyth/app.py
+++ tmp_pyth/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+tmp_pyth API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="tmp_pyth",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://tmp_pyth.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "niski1996",
+        "url": "http://tmp_pyth.com/contact/",
+        "email": "niski1996@tmp_pyth.com",
     },
     license_info={
         "name": "The Unlicense",
